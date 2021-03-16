c6a1fbb887339130ea545340d2af9a46e98431ac
opeyemidy <opeyemidy@gmail.com>
2021-03-16 16:10:54 +0100

    restructure route

---
+++ src/components/footer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function header() {
+  return <div>this is footer</div>;
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/header';
+import Footer from './components/footer';
+import About from './pages/about';
+import Service from './pages/service';
+import User from './pages/User';
+
+const Routing = () => {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/about" component={About} />
+        <Route path="/service" component={Service} />
+        <Route path="/:id" component={User} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routing />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
